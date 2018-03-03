373175f070af28569aa0cc8ecc8ce34bcecc3b0e
hhlTer <grzc@ukr.net>
2018-03-03 13:21:35 +0200

    im try

--- src/main/java/model/tempRealization/UnirestSerialization.java
+++ src/main/java/model/tempRealization/UnirestSerialization.java
@@ -1,16 +1,16 @@
 package model.tempRealization;
-import com.mashape.unirest.http.ObjectMapper;
-import com.mashape.unirest.http.Unirest;
+//import com.mashape.unirest.http.ObjectMapper;
+//import com.mashape.unirest.http.Unirest;
 
     class UnirestSerialization {
         static {
-            Unirest.setObjectMapper(new ObjectMapper() {
-                public <T> T readValue(String value, Class<T> valueType) {
-                    return JSON.parseObject(value, valueType);
-                }
-                public String writeValue(Object value) {
-                    return JSON.toJSONString(value);
-                }
-            });
+//            Unirest.setObjectMapper(new ObjectMapper() {
+//                public <T> T readValue(String value, Class<T> valueType) {
+//                    return JSON.parseObject(value, valueType);
+//                }
+//                public String writeValue(Object value) {
+//                    return JSON.toJSONString(value);
+//                }
+//            });
         }
     }
