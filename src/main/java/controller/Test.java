0a7d6173ee72abc37e259b42c6f00102dbb7cd61
DmitriyVusyk <kotmurkot4502@gmail.com>
2018-03-09 19:18:13 +0100

    Merge branch 'master' of /Users/dmytrovusyk/Documents/GoJavaOnline/ua.goit.comfinalproject.gojava10.groupe2 with conflicts.

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -3,8 +3,10 @@ package controller;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
 import model.tempController.QueryFromYoutube;
+import model.tempRealization.YouTubeAPI;
 import model.youTubeDataContainer.GeneralDataContainer;
 import model.youTubeDataContainer.RelatedPlaylists;
+import model.youTubeDataContainer.Responce;
 
 import java.util.Arrays;
 
@@ -14,29 +16,19 @@ public class Test {
     private static String channelId3 = "UCUoEWMvijAEn-hWTLUtfdoA";
 
     public static void main(String[] args) throws UnirestException {
-
-
-
         QueryFromYoutube qfy = new QueryFromYoutube();
         GeneralDataContainer gdc = new GeneralDataContainer();
-
-
-        qfy.makeQuary(gdc, "UC8mndg3Tr2g9iMoJMI4ob5A");
-        System.out.println(gdc);
-
-//        MainController mc = new MainController();
-//        String[] ids = {channelId, channelId2, channelId3};
-//        RelatedPlaylists rp = new RelatedPlaylists();
-//
+        MainController mc = new MainController();
+        String[] ids = {channelId, channelId2, channelId3};
+        RelatedPlaylists rp = new RelatedPlaylists();
         // System.out.println(mc.sortChannels(ids).toString());
 
 
-//        YouTubeAPI.search("UC8mndg3Tr2g9iMoJMI4ob5A");
-//        System.out.println(gdc.getUploads());
-//        qfy.makeQuary(gdc, "UC8mndg3Tr2g9iMoJMI4ob5A");
-//        System.out.println(gdc.getUploads());
-//        qfy.makeQueryForChennelsID(gdc,gdc.getUploads());
-//        System.out.println(Arrays.toString(gdc.getVideoIds()));
+        System.out.println(gdc.getUploads());
+        qfy.makeQuary(gdc, "UC8mndg3Tr2g9iMoJMI4ob5A");
+        System.out.println(gdc.getUploads());
+        qfy.makeQueryForChennelsID(gdc,gdc.getUploads());
+        System.out.println(Arrays.toString(gdc.getVideoIds()));
         //YouTubeAPI.getVideoList();
     }
 
