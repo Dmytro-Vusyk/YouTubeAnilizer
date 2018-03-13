d458b89018fb05fd6485597262a0b76605a2e9f8
DmitriyVusyk <kotmurkot4502@gmail.com>
2018-03-13 10:27:29 +0100

    FXMLSettingsController - кнопа шукать

--- src/main/java/view/FXMLSettingsController.java
+++ src/main/java/view/FXMLSettingsController.java
@@ -8,8 +8,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,13 +34,32 @@ public class FXMLSettingsController extends FXMLDocumentController implements In
     @FXML
     private JFXCheckBox isUsingCash;
 
+    final DirectoryChooser directoryChooser = new DirectoryChooser();
+
+    private void configuringDirectoryChooser(DirectoryChooser directoryChooser) {
+        // Set title for DirectoryChooser
+        directoryChooser.setTitle("Select Directories");
+
+        // Set Initial Directory
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+
     @FXML
     void onActionBtnBrowse(ActionEvent event) {
-
+        configuringDirectoryChooser(directoryChooser);
+        File dir = directoryChooser.showDialog(new Stage().getOwner());
+        if (dir != null) {
+            textFieldAdress.setText(dir.getAbsolutePath());
+        } else {
+            textFieldAdress.setText(null);
+        }
+        System.out.println(textFieldAdress);
     }
 
     @FXML
     void ionActionUsingCash(ActionEvent event) {
+
+
 
     }
 
